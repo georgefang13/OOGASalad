1b2be5123952cedadbaa79815dc21d73c51a7cc4
Ethan Horowitz <ethan.horowitz@duke.edu>
2023-03-27 22:04:01 -0400

    created BoardGraph class with tests

---
+++ src/test/java/oogasalad/board/BoardGraphTest.java
@@ -0,0 +1,138 @@
+package oogasalad.board;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BoardGraphTest {
+
+    private BoardGraph g;
+
+    // create new instance of test object before each test is run
+    @BeforeEach
+    void setup () {
+        g = new BoardGraph();
+    }
+
+    @Test
+    void addMultipleNodes(){
+        g.addNode("A");
+        g.addNode("B");
+        g.addNode("C");
+
+        assertEquals("A", g.getNode("A").getId());
+        assertEquals("B", g.getNode("B").getId());
+        assertEquals("C", g.getNode("C").getId());
+        assertEquals(3, g.getNodes().size());
+    }
+
+    @Test
+    void addExistingNodes(){
+        assertTrue(g.addNode("A"));
+        assertTrue(g.addNode("B"));
+        assertTrue(g.addNode("C"));
+
+        assertFalse(g.addNode("A"));
+        assertFalse(g.addNode("B"));
+        assertFalse(g.addNode("C"));
+
+        assertEquals("A", g.getNode("A").getId());
+        assertEquals("B", g.getNode("B").getId());
+        assertEquals("C", g.getNode("C").getId());
+        assertEquals(3, g.getNodes().size());
+    }
+
+    ArrayList<Map.Entry<String, BoardGraph.BoardGraphNode>> getSortedEdges(BoardGraph.BoardGraphNode node){
+        ArrayList<Map.Entry<String, BoardGraph.BoardGraphNode>> sortedEdges = new ArrayList<>(node.getEdges().entrySet());
+        sortedEdges.sort(Comparator.comparing(e -> e.getValue().getId()));
+        return sortedEdges;
+    }
+
+    @Test
+    void addConnections(){
+        g.addNode("A");
+        g.addNode("B");
+        g.addNode("C");
+
+        assertTrue(g.addConnection("A", "B", "AB"));
+        assertTrue(g.addConnection("B", "C", "BC"));
+        assertTrue(g.addConnection("C", "A", "CA"));
+
+        assertEquals("A", g.getNode("A").getId());
+        assertEquals("B", g.getNode("B").getId());
+        assertEquals("C", g.getNode("C").getId());
+        assertEquals(3, g.getNodes().size());
+
+        assertEquals("B", getSortedEdges(g.getNode("A")).get(0).getValue().getId());
+        assertEquals("C", getSortedEdges(g.getNode("B")).get(0).getValue().getId());
+        assertEquals("A", getSortedEdges(g.getNode("C")).get(0).getValue().getId());
+
+        assertEquals("AB", getSortedEdges(g.getNode("A")).get(0).getKey());
+        assertEquals("BC", getSortedEdges(g.getNode("B")).get(0).getKey());
+        assertEquals("CA", getSortedEdges(g.getNode("C")).get(0).getKey());
+    }
+
+    @Test
+    void addNullConnections(){
+        try {
+            g.addNode(null);
+        }
+        catch (IllegalArgumentException e){
+            assertEquals("Node name cannot be null", e.getMessage());
+        }
+
+        g.addNode("A");
+        g.addNode("B");
+        g.addNode("C");
+
+        try { g.addConnection(null, "B", "AB"); }
+        catch (IllegalArgumentException e){
+            assertEquals("Node name cannot be null", e.getMessage());
+        }
+        try { g.addConnection("A", null, "AB"); }
+        catch (IllegalArgumentException e) {
+            assertEquals("Node name cannot be null", e.getMessage());
+        }
+        try { g.addConnection("A", "B", null); }
+        catch (IllegalArgumentException e){
+            assertEquals("Edge name cannot be null", e.getMessage());
+        }
+
+        g.addConnection("A", "B", "AB");
+        g.addConnection("B", "C", "BC");
+        g.addConnection("C", "A", "CA");
+
+        assertFalse(g.addConnection("A", "B", "AB"));
+        assertFalse(g.addConnection("B", "C", "BC"));
+        assertFalse(g.addConnection("C", "A", "CA"));
+
+        assertTrue(g.addConnection("B", "A", "BA"));
+    }
+
+    @Test
+    void addRedundantEdges(){
+        g.addNode("A");
+        g.addNode("B");
+        g.addNode("C");
+
+        g.addConnection("A", "B", "AB");
+        g.addConnection("B", "C", "BC");
+        g.addConnection("C", "A", "CA");
+
+        assertFalse(g.addConnection("A", "B", "AB"));
+        assertFalse(g.addConnection("B", "C", "BC"));
+        assertFalse(g.addConnection("C", "A", "CA"));
+
+        assertFalse(g.addConnection("A", "B", "AB2"));
+        assertFalse(g.addConnection("B", "C", "BC2"));
+        assertFalse(g.addConnection("C", "A", "CA2"));
+
+        assertTrue(g.addConnection("B", "A", "BA"));
+    }
+
+}
