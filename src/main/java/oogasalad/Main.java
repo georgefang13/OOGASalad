f31ac0998de8ab5fae0cf99cb42a5c3031f680ae
George Fang <gyf2@duke.edu>
2023-04-04 16:56:30 -0400

    Saving so I can checkout master. Added start to main

--- src/main/java/oogasalad/Main.java
+++ src/main/java/oogasalad/Main.java
@@ -1,6 +1,11 @@
 package oogasalad;
 
 
+import static javafx.application.Application.launch;
+
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 /**
  * Feel free to completely change this code or delete it entirely. 
  */
@@ -16,8 +21,23 @@ public class Main {
      * Start of the program.
      */
 
+//    public static void main (String[] args) {
+//        Main m = new Main();
+//        System.out.println(m.getVersion());
+//    }
+
     public static void main (String[] args) {
+        launch(args);
+        // from Duvall below
         Main m = new Main();
         System.out.println(m.getVersion());
+    }
+
+    public void start(Stage stage) throws Exception {
+        Button button = new Button("Open Modal");
+        button.setOnAction(e -> {
+            AModal modal = new Modal();
+            modal.showAndWait();
+        });
     }
 }
