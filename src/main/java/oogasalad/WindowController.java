ae6129896a7185d56d08279d0a7baf74af6deb8e
Connor Wells-Weiner <cjw84@duke.edu>
2023-04-04 22:28:35 -0400

    Mappings between Window and ID

--- src/main/java/oogasalad/WindowController.java
+++ src/main/java/oogasalad/WindowController.java
@@ -1,6 +1,10 @@
 package oogasalad;
 
+import java.util.Map;
+
 public class WindowController implements WindowMediator {
+  private Map<String, Map<String, AbstractWindow>> windowMap;
+  private Map<String, Integer> windowCounters;
 
   @Override
   public String registerWindow(String windowType) {
@@ -9,8 +13,7 @@ public class WindowController implements WindowMediator {
     String windowId = windowType + "_" + counter;
     windowMap.computeIfAbsent(windowType, k -> new HashMap<>()).put(windowId, window);
     windowCounters.put(windowType, counter);
-    return windowId;
-    return null;
+    return windowID;
   }
 
   @Override
