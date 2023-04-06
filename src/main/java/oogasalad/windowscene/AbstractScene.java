0965378f707078c73e0a90a98c6b71c7210635e5
Connor Wells-Weiner <cjw84@duke.edu>
2023-04-05 21:30:26 -0400

    Started adding language and resource customizability

--- src/main/java/oogasalad/windowscene/AbstractScene.java
+++ src/main/java/oogasalad/windowscene/AbstractScene.java
@@ -1,18 +1,41 @@
 package oogasalad.windowscene;
 
 
+import java.util.ResourceBundle;
 import javafx.scene.Scene;
 
 public abstract class AbstractScene {
 
   protected AbstractWindow window;
+
   protected Scene scene;
+  protected ResourceBundle textResources;
+  protected ResourceBundle numericResources;
+  protected String stylesheet;
 
   public AbstractScene(AbstractWindow window) {
     this.window = window;
+    setTextResources("english.properties");
+    setNumericResources("numeric.properties");
+    setTheme("default.css");
   }
 
   public abstract Scene makeScene();
+
+  public void setTheme(String theme) {
+    stylesheet = theme + ".css";
+    updateStylesheet();
+  }
+
+  public void setTextResources(String resource) {
+    textResources = ResourceBundle.getBundle(resource);
+    updateTextResources();
+  }
+
+  public void setNumericResources(String resource) {
+    numericResources = ResourceBundle.getBundle(resource);
+    updateNumericResources();
+  }
 
   protected Scene getScene() {
     return scene;
@@ -25,4 +48,14 @@ public abstract class AbstractScene {
   protected void setScene(Scene scene) {
     this.scene = scene;
   }
+  
+  protected abstract void updateTextResources();
+
+  protected abstract void updateNumericResources();
+
+  protected void updateStylesheet() {
+    scene.getStylesheets().clear();
+    scene.getStylesheets().add(stylesheet);
+  }
+
 }
