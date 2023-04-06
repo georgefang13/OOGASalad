9101e8d65ad0e9421f05c58d70d7305279f6defd
Rodrigo Bassi Guerreiro <rb419@duke.edu>
2023-04-06 11:44:49 -0400

    added base methods for rule

--- src/main/java/oogasalad/gamerunner/backend/Rule.java
+++ src/main/java/oogasalad/gamerunner/backend/Rule.java
@@ -1,17 +1,31 @@
 package oogasalad.gamerunner.backend;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import oogasalad.gamerunner.backend.interpreter.Interpreter;
+
 /**
  * @author Rodrigo
  */
 public class Rule {
-  // interpreter instance variable
+  List<String> myInstructions;
 
-  public Rule(String instructions) {
-    // initialize interpreter
-    // pass instructions into interpreter
+  public Rule() {
+    myInstructions = new ArrayList<>();
   }
 
-  public void enforce() {
+  public void addInstruction(String instruction) {
+    myInstructions.add(instruction);
+  }
 
+  public Iterable<String> getInstructions() {
+    return Collections.unmodifiableList(myInstructions);
+  }
+
+  public void enforce(Interpreter interpreter) {
+    for (String instruction : myInstructions) {
+      interpreter.interpret(instruction);
+    }
   }
 }
