e7d14b53533e8f22c9fdd5acd686e384a1dab1ed
Rodrigo Bassi Guerreiro <rb419@duke.edu>
2023-04-06 02:46:46 -0400

    added rule pseudocode

---
+++ src/main/java/oogasalad/gamerunner/backend/Rule.java
@@ -0,0 +1,17 @@
+package oogasalad.gamerunner.backend;
+
+/**
+ * @author Rodrigo
+ */
+public class Rule {
+  // interpreter instance variable
+
+  public Rule(String instructions) {
+    // initialize interpreter
+    // pass instructions into interpreter
+  }
+
+  public void enforce() {
+
+  }
+}
