fe302c783c959651412cbc380e9d2c034402531f
Connor Wells-Weiner <cjw84@duke.edu>
2023-04-05 20:39:10 -0400

    Merging

--- src/main/java/oogasalad/Main.java
+++ src/main/java/oogasalad/Main.java
@@ -5,7 +5,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
   @Override
   public void start(Stage primaryStage) throws Exception {
     WindowMediator mediator = new WindowController();
