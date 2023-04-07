a0958050d04029dff629cd1beff54b2771cd5618
Joao Carvalho <joao.carvalho@duke.edu>
2023-04-06 23:49:43 -0400

    Added the node editor classes

---
+++ src/main/java/oogasalad/frontend/nodeEditor/skins/SkinController.java
@@ -0,0 +1,43 @@
+package oogasalad.frontend.nodeEditor.skins;
+import javafx.geometry.Side;
+
+/**
+ * Responsible for skin-specific logic in the graph editor demo.
+ */
+public interface SkinController {
+
+    /**
+     * Adds a node to the graph.
+     *
+     * @param currentZoomFactor the current zoom factor (1 for 100%)
+     */
+    void addNode(final double currentZoomFactor);
+
+    /**
+     * activates this skin
+     */
+    void activate();
+
+    /**
+     * Adds a connector of the given type to all selected nodes.
+     *
+     * @param position the currently selected connector position
+     * @param input {@code true} for input, {@code false} for output
+     */
+    void addConnector(Side position, boolean input);
+
+    /**
+     * Clears all connectors from all selected nodes.
+     */
+    void clearConnectors();
+
+    /**
+     * Handles the paste operation.
+     * @param selectionCopier {@link SelectionCopier}
+     */
+
+    /**
+     * Handles the select-all operation.
+     */
+    void handleSelectAll();
+}
