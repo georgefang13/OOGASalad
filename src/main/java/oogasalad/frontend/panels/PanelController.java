709c46031c01857a3b980898db451e4d7c5de931
Owen MacKenzie <opm7@duke.edu>
2023-04-09 19:12:45 -0400

    panel controller edits

--- src/main/java/oogasalad/frontend/panels/PanelController.java
+++ src/main/java/oogasalad/frontend/panels/PanelController.java
@@ -1,4 +1,14 @@
 package oogasalad.frontend.panels;
 
+import oogasalad.frontend.scenes.SceneController;
+
+import java.util.Map;
+
 public class PanelController {
+    private Map<String,Panel> panelMap;
+    private SceneController sceneController;
+
+
+
+
 }
