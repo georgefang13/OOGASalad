797455551dbe847368a5e4417cb2f412ba15e2c7
Rodrigo Bassi Guerreiro <rb419@duke.edu>
2023-04-12 00:22:45 -0400

    merged interpreter branch, updated Goal

--- src/main/java/oogasalad/gamerunner/backend/interpretables/Goal.java
+++ src/main/java/oogasalad/gamerunner/backend/interpretables/Goal.java
@@ -1,5 +1,29 @@
 package oogasalad.gamerunner.backend.interpretables;
 
-public class Goal {
+import oogasalad.gameeditor.backend.id.IdManager;
+import oogasalad.gamerunner.backend.interpreter.Interpreter;
+import oogasalad.sharedDependencies.backend.ownables.variables.Variable;
+
+/**
+ * Defines a set of instructions that check whether some condition in the game has been met,
+ * establishing some outcome (some player wins or loses)
+ *
+ * @author Ethan Horowitz
+ * @author Rodrigo Bassi Guerreiro
+ */
+public class Goal extends Interpretable {
+  public boolean test(Interpreter interpreter, IdManager idmanager){
+    interpreter.interpret("del :game_output_state");
+
+    for (String instruction : getInstructions()) {
+      interpreter.interpret(instruction);
+    }
+
+    if (!idmanager.isIdInUse("output_state")){
+      return false;
+    }
+    Variable v = (Variable) idmanager.getObject("output_state");
+    return v.get().equals(true);
+  }
 
 }
