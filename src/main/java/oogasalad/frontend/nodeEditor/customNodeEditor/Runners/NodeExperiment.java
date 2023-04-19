20a2fc5316fb8bd7a3a0d3129a33be8ef89a51d5
Connor Wells-Weiner <cjw84@duke.edu>
2023-04-19 02:01:36 -0400

    Modifying imageview.

--- src/main/java/oogasalad/frontend/nodeEditor/customNodeEditor/Runners/NodeExperiment.java
+++ src/main/java/oogasalad/frontend/nodeEditor/customNodeEditor/Runners/NodeExperiment.java
@@ -5,7 +5,6 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -19,23 +18,18 @@ import oogasalad.frontend.nodeEditor.customNodeEditor.Nodes.SumNode;
 
 public class NodeExperiment extends Application {
 
-  private double initialScale = 1.0;
-
   @Override
   public void start(Stage primaryStage) {
     ImageView imageView = new ImageView(
         "https://static.vecteezy.com/system/resources/previews/005/424/896/original/blueprint-background-in-blue-planning-architecture-sheet-with-grid-free-vector.jpg");
     DraggableAbstractNode sum = new SumNode(100, 100, 50, 25, "red");
     Group group = new Group(imageView, sum);
-    initialScale = group.getScaleX();
-    Pane content = new Pane(group);
-    content.setPrefSize(800, 600);
-    imageView.fitWidthProperty().bind(content.widthProperty());
-    imageView.fitHeightProperty().bind(content.heightProperty());
+    StackPane content = new StackPane(new Group(group));
+    content.setMinSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
     content.setOnScroll(e -> {
       if (e.isShortcutDown() && e.getDeltaY() != 0) {
         if (e.getDeltaY() < 0) {
-          group.setScaleX(Math.max(group.getScaleX() - 0.1, initialScale));
+          group.setScaleX(Math.max(group.getScaleX() - 0.1, 0.5));
         } else {
           group.setScaleX(Math.min(group.getScaleX() + 0.1, 5.0));
         }
@@ -53,3 +47,4 @@ public class NodeExperiment extends Application {
     primaryStage.show();
   }
 }
+
