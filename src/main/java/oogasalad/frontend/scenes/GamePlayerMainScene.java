f3a85677c1940185010f001630d621471ad34c4e
Owen MacKenzie <opm7@duke.edu>
2023-04-20 16:50:27 -0400

    modified flush before 420

--- src/main/java/oogasalad/frontend/scenes/GamePlayerMainScene.java
+++ src/main/java/oogasalad/frontend/scenes/GamePlayerMainScene.java
@@ -1,5 +1,6 @@
 package oogasalad.frontend.scenes;
 
+import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -27,6 +28,7 @@ public class GamePlayerMainScene extends AbstractScene {
   private String playerTurn;
   private Label textInstructions;
   private BorderPane root;
+  private GridPane boardPane;
   private VBox textVBOX;
   private String instruction;
   private GameRunnerController gameRunnerController;
@@ -80,16 +82,24 @@ public class GamePlayerMainScene extends AbstractScene {
 
   private void placePieceOnGrid(BoardPiece activePiece){
     if (activePiece.getName().equals(playerTurn)){
-      activePiece.
+      Point2D localCoordinates = boardPane.sceneToLocal(activePiece.getNode().getLayoutX(), activePiece.getNode().getLayoutY());
+      double x = localCoordinates.getX();
+      double y = localCoordinates.getY();
     }
   }
 
   private void initializeBoard() {
-    GridPane boardPane = board.getBoardVisual();
+    boardPane = board.getBoardVisual();
     boardPane.setOnMouseClicked((MouseEvent event) -> {
       double x = event.getX();
       double y = event.getY();
-      runOnClick(x, y);
+      Board.BoardXY boardXY = board.boardXYofClick(x, y);
+      int boardX = boardXY.x();
+      int boardY = boardXY.y();
+      String inputText = boardY + "," + boardX;
+      parseResponse(gameRunnerController.userResponds(inputText));
+      refreshInstructions();
+      //runOnClick(x, y);
     });
     VBox boardVBOX = new VBox(boardPane);
     boardVBOX.setAlignment(Pos.CENTER);
@@ -103,7 +113,19 @@ public class GamePlayerMainScene extends AbstractScene {
     BoardPiece O1 = new BoardPiece("O",1);
     O1.setSize(30);
     newPieces.add(O1);
-    root.getChildren().addAll(x1.getNode(),O1.getNode());
+    BoardPiece x2 = new BoardPiece("X",2);
+    x2.setSize(30);
+    newPieces.add(x1);
+    BoardPiece O2 = new BoardPiece("O",2);
+    O2.setSize(30);
+    newPieces.add(O1);
+    BoardPiece x3 = new BoardPiece("X",3);
+    x3.setSize(30);
+    newPieces.add(x1);
+    BoardPiece O3 = new BoardPiece("O",3);
+    O3.setSize(30);
+    newPieces.add(O1);
+    root.getChildren().addAll(x1.getNode(),O1.getNode(),x2.getNode(),O2.getNode(),x3.getNode(),O3.getNode());
     return newPieces;
   }
 
