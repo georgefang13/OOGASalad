c1ed976cba0d09dfe034941cbd8e69f1822bd6c9
George Fang <gyf2@duke.edu>
2023-04-23 14:25:14 -0400

    Created the game box for selecting games in the gamelibrary. styles are implemented and so far the default image if the upload image pic so when you press it you can choose what image you want to have for the game box

--- src/main/java/oogasalad/frontend/panels/libraryPanels/LibraryGridPanel.java
+++ src/main/java/oogasalad/frontend/panels/libraryPanels/LibraryGridPanel.java
@@ -1,8 +1,14 @@
 package oogasalad.frontend.panels.libraryPanels;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -12,6 +18,11 @@ import oogasalad.frontend.nodeEditor.GraphEditorTutorial;
 import oogasalad.frontend.panels.Panel;
 
 public class LibraryGridPanel extends GridPane implements Panel {
+  private static final ResourceBundle ID_BUNDLE = ResourceBundle.getBundle(
+      "frontend/properties/StylingIDs/CSS_ID");
+  private static final String GAME_BOX_ID = "GameBoxID";
+  private final List<String> libraryGames = new ArrayList<>(
+      Arrays.asList("Tic-Tak-Toe", "Fortnite", "Backgammon", "Chess", "Checkers")); //TODO: TEMPORARY
 
   /**
    * Constructor for the environment panel
@@ -35,21 +46,23 @@ public class LibraryGridPanel extends GridPane implements Panel {
     this.getColumnConstraints().addAll(column1, column2, column3, column4);
 
     // populate the grid with labels
-    String[] items = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
-    int numRows = (int) Math.ceil((double) items.length / 4);
-    int rowIndex = 0;
-    int columnIndex = 0;
-    for (String item : items) {
-      Label label = new Label(item);
-      GridPane.setHgrow(label, Priority.ALWAYS);
-      GridPane.setVgrow(label, Priority.ALWAYS);
-      this.add(label, columnIndex, rowIndex);
-      columnIndex++;
-      if (columnIndex > 3) {
-        columnIndex = 0;
-        rowIndex++;
-      }
-    }
+//    String[] items = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
+//    int numRows = (int) Math.ceil((double) items.length / 4);
+//    int rowIndex = 0;
+//    int columnIndex = 0;
+//    for (String item : items) {
+//      Label label = new Label(item);
+//      GridPane.setHgrow(label, Priority.ALWAYS);
+//      GridPane.setVgrow(label, Priority.ALWAYS);
+//      this.add(label, columnIndex, rowIndex);
+//      columnIndex++;
+//      if (columnIndex > 3) {
+//        columnIndex = 0;
+//        rowIndex++;
+//      }
+//    }
+    this.add(createGameBox("UploadImage"), 0, 0);
+    this.add(createGameBox("UploadImage"), 1, 0);
 
   }
 
@@ -75,8 +88,13 @@ public class LibraryGridPanel extends GridPane implements Panel {
   public void save() {
 
   }
-  public VBox createGameBox() {
-    //TODO: Make the box that user can click on to open the game.
-    return null;
+  public VBox createGameBox(String gameName) {
+    VBox gameBox = new VBox();
+    ImageView gameImage = new ImageView("file:src/main/resources/frontend/images/GameLibrary/" + gameName + ".png"); //make this a vairable and have the game name find the image name from a helper function
+    gameBox.getChildren().add(gameImage);
+    gameBox.getStyleClass().add(ID_BUNDLE.getString(GAME_BOX_ID));
+
+
+    return gameBox;
   }
 }
