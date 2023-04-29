c3f05a463569854e6d6ffd3219a467b4d3cdd583
hz244 <han.zhang485@duke.edu>
2023-04-29 17:50:00 +0000

    Drop zones

--- src/main/java/oogasalad/Controller/DropZoneController.java
+++ src/main/java/oogasalad/Controller/DropZoneController.java
@@ -1,12 +1,29 @@
 package oogasalad.Controller;
 
-import oogasalad.frontend.components.DropzoneComponent.Dropzone;
+import oogasalad.frontend.components.dropzoneComponent.Dropzone;
 
 public class DropZoneController {
 
   private Dropzone previous;
-  private Dropzone clicked;
+  private Dropzone current;
+
   public DropZoneController(){
+
+  }
+
+  /**
+   * Keep track of previous dropzone to create
+   * @param clicked
+   */
+  public void storePrevious(Dropzone clicked){
+    previous = clicked;
+  }
+
+  /**
+   * storeCurrent keeps track of clicked drop zone to create where the arrow should point
+   * @param current
+   */
+  public void storeCurrent(Dropzone current){
 
   }
 
