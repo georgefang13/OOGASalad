bd68bb19fbcebc65ebcf4a8ca7ac70013e68e007
George Fang <gyf2@duke.edu>
2023-04-29 23:23:56 -0400

    Created the skeleton for the login screen, and the language drop down box. Commiting now so i can refactor our language properties files and the css folder

--- src/main/java/oogasalad/frontend/scenes/SplashMainScene.java
+++ src/main/java/oogasalad/frontend/scenes/SplashMainScene.java
@@ -1,12 +1,17 @@
 package oogasalad.frontend.scenes;
 
-import javafx.geometry.Pos;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import oogasalad.frontend.windows.WindowTypes;
 
 /**
  * @author Connor Wells
@@ -14,8 +19,28 @@ import oogasalad.frontend.windows.WindowTypes;
  */
 
 public class SplashMainScene extends AbstractScene {
-
-  private Button playButton, editButton, modalButton;
+  private static final String LANGUAGE_FILE_PATH = "src/main/resources/frontend/properties/text";
+  private static final String LANGUAGE_PLACEHOLDER = "English";
+  private static final String THEME_FILE_PATH = "src/main/resources/frontend/css";
+  private static final String THEME_PLACEHOLDER = "Light";
+  private static final ResourceBundle ELEMENT_LABELS = ResourceBundle.getBundle(
+      "frontend/properties/text/english");
+  private static final String USERNAME = "Username";
+  private static final String PASSWORD = "Password";
+  private static final String LOGIN = "Login";
+  private static final String SIGN_UP = "SignUp";
+  private static final ResourceBundle ID_BUNDLE = ResourceBundle.getBundle(
+      "frontend/properties/StylingIDs/CSS_ID");
+  private static final String LEFT_HEADER_BOX_ID = "LeftHeaderBoxID";
+  private Label username;
+  private Label password;
+  private TextField usernameField;
+  private TextField passwordField;
+  private Hyperlink login;
+  private Hyperlink signUp;
+  private ComboBox<String> languageDropdown;
+  private ComboBox<String> themeDropdown;
+  private VBox root;
 
   public SplashMainScene(SceneController sceneController) {
     super(sceneController);
@@ -23,47 +48,110 @@ public class SplashMainScene extends AbstractScene {
 
   @Override
   public Scene makeScene() {
-    StackPane root = new StackPane();
-    VBox vbox = new VBox(10);
-    playButton = new Button();
-    playButton.setOnAction(
-        e -> panelController.getSceneController().getWindowController()
-            .registerAndShow(WindowTypes.WindowType.LIBRARY_WINDOW));
-    editButton = new Button();
-    editButton.setOnAction(
-        e -> panelController.getSceneController().getWindowController()
-            .registerAndShow(WindowTypes.WindowType.EDIT_WINDOW));
-    modalButton = new Button(); //ADD NEW WINDOW
-    modalButton.setOnAction(
-        e -> panelController.getSceneController().getWindowController()
-            .registerAndShow(WindowTypes.WindowType.MODAL_WINDOW));
-    ComboBox<String> languageDropdown = new ComboBox<>();
-    languageDropdown.getItems().addAll("english", "spanish");
+//    StackPane root = new StackPane();
+//    VBox vbox = new VBox(10);
+//    playButton = new Button();
+//    playButton.setOnAction(
+//        e -> panelController.getSceneController().getWindowController()
+//            .registerAndShow(WindowTypes.WindowType.LIBRARY_WINDOW));
+//    editButton = new Button();
+//    editButton.setOnAction(
+//        e -> panelController.getSceneController().getWindowController()
+//            .registerAndShow(WindowTypes.WindowType.EDIT_WINDOW));
+//    modalButton = new Button(); //ADD NEW WINDOW
+//    modalButton.setOnAction(
+//        e -> panelController.getSceneController().getWindowController()
+//            .registerAndShow(WindowTypes.WindowType.MODAL_WINDOW));
+//    ComboBox<String> languageDropdown = new ComboBox<>();
+//    languageDropdown.getItems().addAll("english", "spanish");
+//    languageDropdown.getSelectionModel().selectedItemProperty()
+//        .addListener((observable, oldValue, newValue) -> {
+//          getPropertyManager().setTextResources(newValue);
+//        });
+//    languageDropdown.setValue("english");
+//    ComboBox<String> themeDropdown = new ComboBox<>();
+//    themeDropdown.getItems().addAll("light", "dark");
+//    themeDropdown.getSelectionModel().selectedItemProperty()
+//        .addListener((observable, oldValue, newValue) -> {
+//          getThemeManager().setTheme(newValue);
+//        });
+//    themeDropdown.setValue("light");
+//    vbox.getChildren().addAll(playButton, editButton, modalButton, languageDropdown, themeDropdown);
+//    vbox.setAlignment(Pos.CENTER);
+//    root.getChildren().add(vbox);
+//    setScene(new Scene(root));
+//    setText();
+//    setTheme();
+    root = new VBox();
+    makeInputFields();
+    makeLoginSignUpButtons();
+    makeDropDowns();
+    refreshScene();
+    return getScene();
+  }
+  private void refreshScene() {
+    root.getChildren().clear();
+    root.getChildren().addAll(makeInputFields(), makeLoginSignUpButtons(), makeDropDowns());
+    setScene(new Scene(root));
+  }
+  private VBox makeInputFields() {
+    HBox usernameBox = new HBox();
+    username = new Label(ELEMENT_LABELS.getString(USERNAME));
+    usernameField = new TextField();
+    usernameBox.getChildren().addAll(username, usernameField);
+    HBox passwordBox = new HBox();
+    password = new Label(ELEMENT_LABELS.getString(PASSWORD));
+    passwordField = new TextField();
+    passwordBox.getChildren().addAll(password, passwordField);
+    VBox inputFields = new VBox();
+    inputFields.getChildren().addAll(usernameBox, passwordBox);
+    return inputFields;
+  }
+  private HBox makeLoginSignUpButtons() {
+    HBox loginSignUpButtons = new HBox();
+    login = new Hyperlink(ELEMENT_LABELS.getString(LOGIN));
+    signUp = new Hyperlink(ELEMENT_LABELS.getString(SIGN_UP));
+    loginSignUpButtons.getChildren().addAll(login, signUp);
+    return loginSignUpButtons;
+  }
+  private HBox makeDropDowns() {
+    HBox dropDowns = new HBox();
+    languageDropdown = new ComboBox<>();
+    languageDropdown.getItems().addAll(getLanguageFileNames());
     languageDropdown.getSelectionModel().selectedItemProperty()
         .addListener((observable, oldValue, newValue) -> {
           getPropertyManager().setTextResources(newValue);
         });
-    languageDropdown.setValue("english");
-    ComboBox<String> themeDropdown = new ComboBox<>();
+    languageDropdown.setValue(LANGUAGE_PLACEHOLDER);
+    themeDropdown = new ComboBox<>();
     themeDropdown.getItems().addAll("light", "dark");
     themeDropdown.getSelectionModel().selectedItemProperty()
         .addListener((observable, oldValue, newValue) -> {
           getThemeManager().setTheme(newValue);
         });
     themeDropdown.setValue("light");
-    vbox.getChildren().addAll(playButton, editButton, modalButton, languageDropdown, themeDropdown);
-    vbox.setAlignment(Pos.CENTER);
-    root.getChildren().add(vbox);
-    setScene(new Scene(root));
-    setText();
-    setTheme();
-    return getScene();
+    dropDowns.getChildren().addAll(languageDropdown, themeDropdown);
+    return dropDowns;
+  }
+  private List<String> getLanguageFileNames() {
+    List<String> propertyFileNames = new ArrayList<>();
+    String directory = LANGUAGE_FILE_PATH;
+    File dir = new File(directory);
+    File[] files = dir.listFiles();
+    if (files != null) {
+      for (File file : files) {
+        if (file.isFile()) {
+          String propertyFileName = file.getName().substring(0, file.getName().indexOf("."));
+          propertyFileNames.add(propertyFileName);
+        }
+      }
+    }
+    propertyFileNames.remove("ComponentsFactory"); //TODO: put the language in these files into englihs and spansih
+    propertyFileNames.remove("Modals");
+    return propertyFileNames;
   }
 
   @Override
   public void setText() {
-    playButton.setText(getPropertyManager().getText("SplashMainScene.PlayGameButton"));
-    editButton.setText(getPropertyManager().getText("SplashMainScene.EditGameButton"));
-    modalButton.setText(getPropertyManager().getText("SplashMainScene.ModalButton"));
   }
 }
