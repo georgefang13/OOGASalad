ebd8f6670e50a21ec713023336a12c9135a1c6d7
George Fang <gyf2@duke.edu>
2023-04-30 02:31:43 -0400

    Finished the new splash screen in all of its glory

--- src/main/java/oogasalad/frontend/scenes/SplashMainScene.java
+++ src/main/java/oogasalad/frontend/scenes/SplashMainScene.java
@@ -4,18 +4,15 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import oogasalad.frontend.modals.subDisplayModals.AlertModal;
-import oogasalad.frontend.windows.LibraryWindow;
-import oogasalad.frontend.windows.LibraryWindow.WindowScenes;
 import oogasalad.frontend.windows.WindowTypes.WindowType;
 import oogasalad.sharedDependencies.backend.database.Database;
 import oogasalad.sharedDependencies.backend.database.UserManager;
@@ -27,20 +24,29 @@ import oogasalad.sharedDependencies.backend.database.UserManager;
 
 public class SplashMainScene extends AbstractScene {
   private static final String LANGUAGE_DIRECTORY_PATH = "src/main/resources/frontend/properties/text";
-  private static final String LANGUAGE_PLACEHOLDER = "English";
+  private static final String LANGUAGE_PLACEHOLDER = "english";
   private static final String THEME_DIRECTORY_PATH = "src/main/resources/frontend/css";
-  private static final String THEME_PLACEHOLDER = "Light";
+  private static final String THEME_PLACEHOLDER = "light";
   private static final ResourceBundle ELEMENT_LABELS = ResourceBundle.getBundle(
       "frontend/properties/text/english");
   private static final String USERNAME = "Username";
   private static final String PASSWORD = "Password";
   private static final String LOGIN = "Login";
   private static final String SIGN_UP = "SignUp";
+  private static final String TITLE = "Title";
   private static final ResourceBundle ID_BUNDLE = ResourceBundle.getBundle(
       "frontend/properties/StylingIDs/CSS_ID");
-  private static final String LEFT_HEADER_BOX_ID = "LeftHeaderBoxID";
-  private Label username;
-  private Label password;
+  private static final String SPLASH_ROOT_ID = "SplashRootID";
+  private static final String TITLE_ID = "TitleID";
+  private static final String USERNAME_PASSWORD_BOX_ID = "UsernamePasswordBoxID";
+  private static final String INPUT_FIELDS_ID = "InputFieldsID";
+  private static final String USERNAME_PASSWORD_LABEL_ID = "UsernameLabelID";
+  private static final String USERNAME_PASSWORD_FIELD_ID = "UsernameFieldID";
+  private static final String LOGIN_SIGNUP_ID = "LoginID";
+  private static final String DROPDOWN_ID = "DropdownID";
+private static final String DROPDOWN_BOX_ID = "DropdownBoxID";
+  private Label usernameLabel;
+  private Label passwordLabel;
   private TextField usernameField;
   private TextField passwordField;
   private Hyperlink login;
@@ -48,6 +54,7 @@ public class SplashMainScene extends AbstractScene {
   private ComboBox<String> languageDropdown;
   private ComboBox<String> themeDropdown;
   private VBox root;
+  private HBox temp;
   private UserManager userManager;
 
   public SplashMainScene(SceneController sceneController) {
@@ -56,69 +63,54 @@ public class SplashMainScene extends AbstractScene {
 
   @Override
   public Scene makeScene() {
-//    StackPane root = new StackPane();
-//    VBox vbox = new VBox(10);
-//    playButton = new Button();
-//    playButton.setOnAction(
-//        e -> panelController.getSceneController().getWindowController()
-//            .registerAndShow(WindowTypes.WindowType.LIBRARY_WINDOW));
-//    editButton = new Button();
-//    editButton.setOnAction(
-//        e -> panelController.getSceneController().getWindowController()
-//            .registerAndShow(WindowTypes.WindowType.EDIT_WINDOW));
-//    modalButton = new Button(); //ADD NEW WINDOW
-//    modalButton.setOnAction(
-//        e -> panelController.getSceneController().getWindowController()
-//            .registerAndShow(WindowTypes.WindowType.MODAL_WINDOW));
-//    ComboBox<String> languageDropdown = new ComboBox<>();
-//    languageDropdown.getItems().addAll("english", "spanish");
-//    languageDropdown.getSelectionModel().selectedItemProperty()
-//        .addListener((observable, oldValue, newValue) -> {
-//          getPropertyManager().setTextResources(newValue);
-//        });
-//    languageDropdown.setValue("english");
-//    ComboBox<String> themeDropdown = new ComboBox<>();
-//    themeDropdown.getItems().addAll("light", "dark");
-//    themeDropdown.getSelectionModel().selectedItemProperty()
-//        .addListener((observable, oldValue, newValue) -> {
-//          getThemeManager().setTheme(newValue);
-//        });
-//    themeDropdown.setValue("light");
-//    vbox.getChildren().addAll(playButton, editButton, modalButton, languageDropdown, themeDropdown);
-//    vbox.setAlignment(Pos.CENTER);
-//    root.getChildren().add(vbox);
-//    setScene(new Scene(root));
-//    setText();
-//    setTheme();
     root = new VBox();
+    root.getStyleClass().add(ID_BUNDLE.getString(SPLASH_ROOT_ID));
     userManager = new UserManager(new Database());
+    makeTitle();
     makeInputFields();
     makeLoginSignUpButtons();
     makeDropDowns();
     refreshScene();
+    setTheme();
     return getScene();
   }
   private void refreshScene() {
     root.getChildren().clear();
-    root.getChildren().addAll(makeInputFields(), makeLoginSignUpButtons(), makeDropDowns());
+    root.getChildren().addAll(makeTitle(), makeInputFields(), makeLoginSignUpButtons(), makeDropDowns());
     setScene(new Scene(root));
+  }
+  private HBox makeTitle() {
+    HBox titleBox = new HBox();
+    Label title = new Label(ELEMENT_LABELS.getString(TITLE));
+    title.getStyleClass().add(ID_BUNDLE.getString(TITLE_ID));
+    titleBox.getChildren().add(title);
+    return titleBox;
   }
   private VBox makeInputFields() {
     HBox usernameBox = new HBox();
-    username = new Label(ELEMENT_LABELS.getString(USERNAME));
+    usernameBox.getStyleClass().add(ID_BUNDLE.getString(USERNAME_PASSWORD_BOX_ID));
+    usernameLabel = new Label(ELEMENT_LABELS.getString(USERNAME));
+    usernameLabel.getStyleClass().add(ID_BUNDLE.getString(USERNAME_PASSWORD_LABEL_ID));
     usernameField = new TextField();
-    usernameBox.getChildren().addAll(username, usernameField);
+    usernameField.getStyleClass().add(ID_BUNDLE.getString(USERNAME_PASSWORD_FIELD_ID));
+    usernameBox.getChildren().addAll(usernameLabel, usernameField);
     HBox passwordBox = new HBox();
-    password = new Label(ELEMENT_LABELS.getString(PASSWORD));
+    passwordBox.getStyleClass().add(ID_BUNDLE.getString(USERNAME_PASSWORD_BOX_ID));
+    passwordLabel = new Label(ELEMENT_LABELS.getString(PASSWORD));
+    passwordLabel.getStyleClass().add(ID_BUNDLE.getString(USERNAME_PASSWORD_LABEL_ID));
     passwordField = new TextField();
-    passwordBox.getChildren().addAll(password, passwordField);
+    passwordField.getStyleClass().add(ID_BUNDLE.getString(USERNAME_PASSWORD_FIELD_ID));
+    passwordBox.getChildren().addAll(passwordLabel, passwordField);
     VBox inputFields = new VBox();
+    inputFields.getStyleClass().add(ID_BUNDLE.getString(INPUT_FIELDS_ID));
     inputFields.getChildren().addAll(usernameBox, passwordBox);
     return inputFields;
   }
   private HBox makeLoginSignUpButtons() {
     HBox loginSignUpButtons = new HBox();
     login = new Hyperlink(ELEMENT_LABELS.getString(LOGIN));
+    login.setUnderline(true);
+    login.getStyleClass().add(ID_BUNDLE.getString(LOGIN_SIGNUP_ID));
     login.setOnMouseClicked(e -> {
       if (userManager.tryLogin(usernameField.getText(), passwordField.getText())) {
         displayLibrary();
@@ -129,6 +121,8 @@ public class SplashMainScene extends AbstractScene {
       }
     });
     signUp = new Hyperlink(ELEMENT_LABELS.getString(SIGN_UP));
+    signUp.setUnderline(true);
+    signUp.getStyleClass().add(ID_BUNDLE.getString(LOGIN_SIGNUP_ID));
     signUp.setOnMouseClicked(e -> {
       if (userManager.tryRegister(usernameField.getText(), passwordField.getText())) {
         displayLibrary();
@@ -148,7 +142,9 @@ public class SplashMainScene extends AbstractScene {
 
   private HBox makeDropDowns() {
     HBox dropDowns = new HBox();
+    dropDowns.getStyleClass().add(ID_BUNDLE.getString(DROPDOWN_BOX_ID));
     languageDropdown = new ComboBox<>();
+    languageDropdown.getStyleClass().add(ID_BUNDLE.getString(DROPDOWN_ID));
     languageDropdown.getItems().addAll(getFileNames(LANGUAGE_DIRECTORY_PATH));
     languageDropdown.getSelectionModel().selectedItemProperty()
         .addListener((observable, oldValue, newValue) -> {
@@ -156,6 +152,7 @@ public class SplashMainScene extends AbstractScene {
         });
     languageDropdown.setValue(LANGUAGE_PLACEHOLDER);
     themeDropdown = new ComboBox<>();
+    themeDropdown.getStyleClass().add(ID_BUNDLE.getString(DROPDOWN_ID));
     themeDropdown.getItems().addAll(getFileNames(THEME_DIRECTORY_PATH));
     themeDropdown.getSelectionModel().selectedItemProperty()
         .addListener((observable, oldValue, newValue) -> {
