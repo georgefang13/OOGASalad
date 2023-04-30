fdcfaa43b8f1e0dac73e7f68e40a240a25e4a3ad
Michael Bryant <mcb115@duke.edu>
2023-04-30 18:17:36 -0400

    shortened main

--- src/main/java/oogasalad/Main.java
+++ src/main/java/oogasalad/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    WindowMediator mediator = new WindowController();
+    new WindowController();
   }
 
   public static void main(String[] args) {
