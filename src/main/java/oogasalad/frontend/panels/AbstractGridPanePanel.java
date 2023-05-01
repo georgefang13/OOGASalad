19cf7e0405dd6721b4ed53ffd67bdddf58d4c74b
George Fang <gyf2@duke.edu>
2023-05-01 00:49:18 -0400

    Finished the scroll pane fix for the library grid panel

--- src/main/java/oogasalad/frontend/panels/AbstractGridPanePanel.java
+++ src/main/java/oogasalad/frontend/panels/AbstractGridPanePanel.java
@@ -1,8 +1,9 @@
 package oogasalad.frontend.panels;
 
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 
-public abstract class AbstractGridPanePanel extends GridPane implements Panel{
+public abstract class AbstractGridPanePanel extends ScrollPane implements Panel{
   private String panelID;
   PanelController panelController;
   public AbstractGridPanePanel(PanelController panelController, String panelID) {
