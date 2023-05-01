f10204d5a1509305a29167b7ce8613b0301f3ab2
Joao Carvalho <joao.carvalho@duke.edu>
2023-04-30 20:44:06 -0400

    Added javadocs for NodeConfiguration and NodeData

--- src/main/java/oogasalad/frontend/nodeEditor/configuration/NodeData.java
+++ src/main/java/oogasalad/frontend/nodeEditor/configuration/NodeData.java
@@ -3,6 +3,7 @@ package oogasalad.frontend.nodeEditor.configuration;
 import java.io.Serializable;
 import java.util.List;
 
+// Represents the data of a node in the node editor as a Java Record
 public record NodeData(String name, String type, List<String> inputs) implements Serializable {
     
 }
