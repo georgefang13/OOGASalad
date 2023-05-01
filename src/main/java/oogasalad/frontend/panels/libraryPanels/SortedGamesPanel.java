0315030bdd51fea0d4b66b4fa1bbdd7944cf5af1
George Fang <gyf2@duke.edu>
2023-04-30 23:39:20 -0400

    Cleaned up the sortedGamesPanel glass

--- src/main/java/oogasalad/frontend/panels/libraryPanels/SortedGamesPanel.java
+++ src/main/java/oogasalad/frontend/panels/libraryPanels/SortedGamesPanel.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.VBox;
 import oogasalad.frontend.panels.Panel;
 import oogasalad.frontend.panels.PanelController;
-import org.checkerframework.checker.units.qual.C;
 
 public class SortedGamesPanel extends VBox implements Panel {
 
@@ -16,12 +15,18 @@ public class SortedGamesPanel extends VBox implements Panel {
   private static final String BOARD_GAMES = ELEMENT_LABELS.getString("BoardGames");
   private static final String CARD_GAMES = ELEMENT_LABELS.getString("CardGames");
   private static final String GRID_GAMES = ELEMENT_LABELS.getString("GridGames");
-  private static final String USER_GAMES = ELEMENT_LABELS.getString("UserGames");
   private static final ResourceBundle ID_BUNDLE = ResourceBundle.getBundle(
       "frontend/properties/StylingIDs/CSS_ID");
   private static final String SORTED_GAMES_VBOX_ID = "SortedGamesVBoxID";
   private static final String SORTED_GAMES_TEXT_ID = "SortedGamesTextID";
   private static final String SORTED_GAMES_TEXT_ALL_ID = "SortedGamesTextAllID";
+  private static final String LIBRARYID = "library";
+  private static final String BOARD_GAME_TAG = "board game";
+  private static final String CARD_GAME_TAG = "card game";
+  private static final String GRID_GAME_TAG = "grid game";
+  private static final String ALL_GAME_TAG = "all";
+
+
   PanelController panelController;
 
 
@@ -50,35 +55,29 @@ public class SortedGamesPanel extends VBox implements Panel {
     sortedGamesVBox.getStyleClass().add(ID_BUNDLE.getString(SORTED_GAMES_VBOX_ID));
     Hyperlink allGames = new Hyperlink(ALL_GAMES);
     allGames.setOnMouseClicked(e -> {
-          panelController.getSceneController().getWindowController().passData("all");
-          panelController.updatePanel("library");
+          panelController.getSceneController().getWindowController().passData(ALL_GAME_TAG);
+          panelController.updatePanel(LIBRARYID);
         });
     allGames.getStyleClass().add(ID_BUNDLE.getString(SORTED_GAMES_TEXT_ALL_ID));
     Hyperlink boardGames = new Hyperlink(BOARD_GAMES);
     boardGames.getStyleClass().add(ID_BUNDLE.getString(SORTED_GAMES_TEXT_ID));
     boardGames.setOnMouseClicked(e -> {
-      panelController.getSceneController().getWindowController().passData("board game");
-      panelController.updatePanel("library");
+      panelController.getSceneController().getWindowController().passData(BOARD_GAME_TAG);
+      panelController.updatePanel(LIBRARYID);
     });
     Hyperlink cardGames = new Hyperlink(CARD_GAMES);
     cardGames.getStyleClass().add(ID_BUNDLE.getString(SORTED_GAMES_TEXT_ID));
     cardGames.setOnMouseClicked(e -> {
-      panelController.getSceneController().getWindowController().passData("card game");
-      panelController.updatePanel("library");
+      panelController.getSceneController().getWindowController().passData(CARD_GAME_TAG);
+      panelController.updatePanel(LIBRARYID);
     });
     Hyperlink gridGames = new Hyperlink(GRID_GAMES);
     gridGames.getStyleClass().add(ID_BUNDLE.getString(SORTED_GAMES_TEXT_ID));
     gridGames.setOnMouseClicked(e -> {
-      panelController.getSceneController().getWindowController().passData("grid game");
-      panelController.updatePanel("library");
+      panelController.getSceneController().getWindowController().passData(GRID_GAME_TAG);
+      panelController.updatePanel(LIBRARYID);
     });
-    Hyperlink userGames = new Hyperlink(USER_GAMES);
-    userGames.getStyleClass().add(ID_BUNDLE.getString(SORTED_GAMES_TEXT_ID));
-    userGames.setOnMouseClicked(e -> {
-      panelController.getSceneController().getWindowController().passData("user"); //TODO: MAKE IT THE USERNAME
-      panelController.updatePanel("library");
-    });
-    sortedGamesVBox.getChildren().addAll(allGames, boardGames, cardGames, gridGames, userGames);
+    sortedGamesVBox.getChildren().addAll(allGames, boardGames, cardGames, gridGames);
     return sortedGamesVBox;
   }
 
