008e868e7c456386551df968075ba7def1df8553
Rodrigo Bassi Guerreiro <rb419@duke.edu>
2023-05-02 23:44:22 -0400

    BUG: frontend/objects.json file ## Description
    Currently the file saving doesn't save to two individual files, as planned, but one file to Components. This needs to be solved so GameRunner can properly instantiate objects. Also, the files aren't formated correctly, with a map tag in front
    
    ## Expected Behavior
    
    When clicking the Save button, the frontend folder inside the game is instantiated, with two folders, Objects.json and Layout.json. The tags are all separated directly under the objects, as displayed in connect4/frontend/objects.json
    
    ## Current Behavior
    
    It makes one folder with a folder called Component.json, and the map tags are still there
    
    ## Steps to Reproduce
    
    Provide detailed steps for reproducing the issue.
    
     1. Add a dropzone in the GameEditor to the screen
     2. Click the save button, at the top header bar on the screen
     3. View the file in Data/Game/(Insert Game Name Here)/frontend/objects.json
    
    ## Failure Logs
    java.lang.NoSuchFieldException: x
    at java.base/java.lang.Class.getDeclaredField(Class.java:2610)
    at oogasalad.frontend.components.AbstractComponent.setValuesfromMap(AbstractComponent.java:48)
    at oogasalad.frontend.components.gameObjectComponent.GameObject.<init>(GameObject.java:39)
    at java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)
    at java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:77)
    at java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)
    at java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:499)
    at java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:480)
    at oogasalad.frontend.components.ComponentsFactory.create(ComponentsFactory.java:76)
    at oogasalad.gameeditor.frontend.ObjectSavingTest.makeComponent(ObjectSavingTest.java:54)
    at oogasalad.gameeditor.frontend.ObjectSavingTest.start(ObjectSavingTest.java:32)
    at org.testfx.framework.junit5.ApplicationAdapter.start(ApplicationAdapter.java:37)
    at org.testfx.toolkit.impl.ApplicationServiceImpl.lambda-bash(ApplicationServiceImpl.java:49)
    at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
    at javafx.graphics/com.sun.javafx.application.PlatformImpl.lambda0(PlatformImpl.java:457)
    at java.base/java.security.AccessController.doPrivileged(AccessController.java:399)
    at javafx.graphics/com.sun.javafx.application.PlatformImpl.lambda1(PlatformImpl.java:456)
    at javafx.graphics/com.sun.glass.ui.InvokeLaterDispatcher.run(InvokeLaterDispatcher.java:96)
    at javafx.graphics/com.sun.glass.ui.win.WinApplication._runLoop(Native Method)
    at javafx.graphics/com.sun.glass.ui.win.WinApplication.lambda(WinApplication.java:184)
    at java.base/java.lang.Thread.run(Thread.java:833)
    
    ## Hypothesis for Fixing the Bug
    
    A previous merge had used a file Strategy to merge stuff, but it didn't get properly merged. This will fix the issue. However, the tags will need to be separated by splitting the map, and making sure the output is consistent to the gameRunner files.

---
+++ src/test/java/oogasalad/gameeditor/controller/ObjectSavingTest.java
@@ -0,0 +1,57 @@
+package oogasalad.gameeditor.controller;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javafx.stage.Stage;
+import oogasalad.Controller.FilesController;
+import oogasalad.frontend.components.Component;
+import oogasalad.frontend.components.ComponentsFactory;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import oogasalad.sharedDependencies.backend.filemanagers.FileManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import util.DukeApplicationTest;
+
+public class ObjectSavingTest extends DukeApplicationTest {
+  private static final String PATH = System.getProperty("user.dir") + "/data/games/Files/frontend/objects.json";
+  FilesController filesController;
+  FileManager fileManager;
+
+  @BeforeEach
+  public void start(Stage stage){
+    System.out.println(PATH);
+    filesController = new FilesController("Files");
+    Component c = makeComponent();
+    filesController.addComponent(c);
+  }
+
+  @Test
+  public void createFile() {
+    assertDoesNotThrow(() -> filesController.saveToFile());
+    assertDoesNotThrow(() -> fileManager = new FileManager(PATH));
+    assertEquals(50, Integer.valueOf(fileManager.getString("TestObject", "x")));
+    assertEquals(50, Integer.valueOf(fileManager.getString("TestObject", "width")));
+    assertTrue(fileManager.getObject(Boolean.class, "TestObject", "unselected", "hasImage"));
+  }
+
+  private static Component makeComponent() {
+    Map<String, String> map = new HashMap<>();
+    map.put("ID", "TestObject");
+    map.put("x", "50");
+    map.put("y", "50");
+    map.put("width", "50");
+    map.put("height", "50");
+    map.put("unselectedhasImage", "50");
+    ComponentsFactory factory = new ComponentsFactory();
+    Component c = factory.create("GameObject", map);
+    return c;
+  }
+}
